a5be406fd5c59ee468383aac3a70fa72a968aa81
Tandith 0005 <tandith0005@gmail.com>
2025-12-07 17:45:04 +0600

    fixed crash

--- src/server.ts
+++ src/server.ts
@@ -35,6 +35,4 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+export default app;
